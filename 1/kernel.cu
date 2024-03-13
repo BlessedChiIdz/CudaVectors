fab53dd29b61829a974c03f9ee38155d6962fc3c
rik51 <animeboy3228@outlook.com>
2024-03-14 03:35:07 +0700

    4

--- 1/kernel.cu
+++ 1/kernel.cu
@@ -5,51 +5,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 // Size of array
-#define N 104857600
 
 
 
 // Kernel
-__global__ void add_vectors(double* a, double* b, double* c)
-{
-	int id = blockDim.x * blockIdx.x + threadIdx.x;
-	if (id < N) c[id] = a[id] + b[id];
-}
+__device__ double globalArr[4][3];
 
+
+__global__ void Kernel() {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 3; j++) {
+			printf("%f ", globalArr[i][j]);
+		}
+		printf("\n");
+	}
+}
 // Main program
 int main()
 {
-	// Number of bytes to allocate for N doubles
-	size_t bytes = N * sizeof(double);
-	int thr_per_blk = 32;
-	// Allocate memory for arrays A, B, and C on host
-	double* A = (double*)malloc(bytes);
-	double* B = (double*)malloc(bytes);
-	double* C = (double*)malloc(bytes);
+	
+	size_t bytes = 3 * 4 * sizeof(double);
+	double TwoDim[3][4];
 
-	// Allocate memory for arrays d_A, d_B, and d_C on device
-	double* d_A, * d_B, * d_C;
-	cudaMalloc(&d_A, bytes);
-	cudaMalloc(&d_B, bytes);
-	cudaMalloc(&d_C, bytes);
-
-	// Fill host arrays A and B
-	for (int i = 0; i < N; i++)
-	{
-		A[i] = 1.0;
-		B[i] = 2.0;
-		
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 4; j++) {
+			TwoDim[i][j] = rand() % 10 + 1;
+		}
 	}
 
-	// Copy data from host arrays A and B to device arrays d_A and d_B
-	cudaMemcpy(d_A, A, bytes, cudaMemcpyHostToDevice);
-	cudaMemcpy(d_B, B, bytes, cudaMemcpyHostToDevice);
+	double TwoDimD[4][3];
 
-	// Set execution configuration parameters
-	//		thr_per_blk: number of CUDA threads per grid block
-	//		blk_in_grid: number of blocks in grid
+	cudaMemcpy(globalArr, TwoDim, bytes,cudaMemcpyHostToDevice);
 	
-	int blk_in_grid = ceil(N / thr_per_blk);
+	
 
 	cudaEvent_t start, stop;
 	cudaEventCreate(&start);
@@ -57,41 +45,14 @@ int main()
 	cudaEventRecord(start, 0);
 
 	// Launch kernel
-	add_vectors <<< blk_in_grid, thr_per_blk >>> (d_A, d_B, d_C);
-
+	
+	Kernel << < 1, 1 >> > ();
 
 	cudaEventRecord(stop, 0);
 	cudaEventSynchronize(stop); // Sync events (BLOCKS till last (stop in this case) has been recorded!)
 	float elapsedTime; // Initialize elapsedTime;
 	cudaEventElapsedTime(&elapsedTime, start, stop);
 
-	// Copy data from device array d_C to host array C
-	cudaMemcpy(C, d_C, bytes, cudaMemcpyDeviceToHost);
-
-	// Verify results
-	double tolerance = 1.0e-14;
-	for (int i = 0; i < N; i++)
-	{
-		//printf("c[%i] = %f\n", i, C[i]);
-	}
-	printf("Execution Time: %f ms", elapsedTime);
-	// Free CPU memory
-	free(A);
-	free(B);
-	free(C);
-
-	// Free GPU memory
-	cudaFree(d_A);
-	cudaFree(d_B);
-	cudaFree(d_C);
-
-	printf("\n---------------------------\n");
-	printf("__SUCCESS__\n");
-	printf("---------------------------\n");
-	printf("N                 = %d\n", N);
-	printf("Threads Per Block = %d\n", thr_per_blk);
-	printf("Blocks In Grid    = %d\n", blk_in_grid);
-	printf("---------------------------\n\n");
-
+	
 	return 0;
 }
