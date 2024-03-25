831fab0c4d42fc4d6f234031d84f1920ae715c20
rik51 <animeboy3228@outlook.com>
2024-03-26 00:08:38 +0700

    4

--- 1/kernel.cu
+++ 1/kernel.cu
@@ -4,55 +4,122 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-// Size of array
+#define BLOCK_DIM 1
+__shared__ float temp[BLOCK_DIM][BLOCK_DIM];
 
-
-
-// Kernel
-__device__ double globalArr[4][3];
-
-
-__global__ void Kernel() {
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 3; j++) {
-			printf("%f ", globalArr[i][j]);
-		}
-		printf("\n");
-	}
-}
-// Main program
-int main()
+__global__ void transposeMatrixFast(float* inputMatrix, float* outputMatrix, int width, int height)
 {
 	
-	size_t bytes = 3 * 4 * sizeof(double);
-	double TwoDim[3][4];
 
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 4; j++) {
-			TwoDim[i][j] = rand() % 10 + 1;
-		}
+	int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
+	int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
+
+	if ((xIndex < width) && (yIndex < height))
+	{
+		int idx = yIndex * width + xIndex;
+
+		temp[threadIdx.y][threadIdx.x] = inputMatrix[idx];
 	}
 
-	double TwoDimD[4][3];
+	__syncthreads();
 
-	cudaMemcpy(globalArr, TwoDim, bytes,cudaMemcpyHostToDevice);
-	
-	
+	xIndex = blockIdx.y * blockDim.y + threadIdx.x;
+	yIndex = blockIdx.x * blockDim.x + threadIdx.y;
 
-	cudaEvent_t start, stop;
-	cudaEventCreate(&start);
-	cudaEventCreate(&stop);
-	cudaEventRecord(start, 0);
+	if ((xIndex < height) && (yIndex < width))
+	{
+		int idx = yIndex * height + xIndex;
 
-	// Launch kernel
-	
-	Kernel << < 1, 1 >> > ();
+		outputMatrix[idx] = temp[threadIdx.x][threadIdx.y];
+	}
+}
 
-	cudaEventRecord(stop, 0);
-	cudaEventSynchronize(stop); // Sync events (BLOCKS till last (stop in this case) has been recorded!)
-	float elapsedTime; // Initialize elapsedTime;
-	cudaEventElapsedTime(&elapsedTime, start, stop);
+__host__ void printMatrixToFile(char* fileName, float* matrix, int width, int height)
+{
+    FILE* file = fopen(fileName, "wt");
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            fprintf(file, "%f\t", matrix[y * width + x]);
+        }
+        fprintf(file, "\n");
+    }
+    fclose(file);
+}
 
-	
-	return 0;
+
+
+
+int main()
+{
+    int width = 3;   
+    int height = 4;
+
+    int matrixSize = width * height;
+    int byteSize = matrixSize * sizeof(float);
+
+    float* inputMatrix = new float[matrixSize];
+    float* outputMatrix = new float[matrixSize];
+
+    for (int i = 0; i < matrixSize; i++)
+    {
+        inputMatrix[i] = i;
+    }
+    int qwe = 0;
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            printf("%f ", inputMatrix[qwe]);
+            qwe++;
+        }
+        printf("\n");
+    }
+    
+    
+        float* devInputMatrix;
+        float* devOutputMatrix;
+
+        cudaMalloc((void**)&devInputMatrix, byteSize);
+        cudaMalloc((void**)&devOutputMatrix, byteSize);
+
+        cudaMemcpy(devInputMatrix, inputMatrix, byteSize, cudaMemcpyHostToDevice);
+
+        dim3 gridSize = dim3(width / BLOCK_DIM, height / BLOCK_DIM, 1);
+        dim3 blockSize = dim3(BLOCK_DIM, BLOCK_DIM, 1);
+
+        cudaEvent_t start;
+        cudaEvent_t stop;
+
+        cudaEventCreate(&start);
+        cudaEventCreate(&stop);
+
+        cudaEventRecord(start, 0);
+
+       
+        
+        transposeMatrixFast << <gridSize, blockSize >> > (devInputMatrix, devOutputMatrix, width, height);
+
+        cudaEventRecord(stop, 0);
+
+        float time = 0;
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&time, start, stop);
+
+        printf("GPU compute time: %.0f\n", time);
+
+        cudaMemcpy(outputMatrix, devOutputMatrix, byteSize, cudaMemcpyDeviceToHost);
+
+        cudaFree(devInputMatrix);
+        cudaFree(devOutputMatrix);
+
+        cudaEventDestroy(start);
+        cudaEventDestroy(stop);
+    
+
+    printMatrixToFile("after.txt", outputMatrix, height, width);
+
+    delete[] inputMatrix;
+    delete[] outputMatrix;
+
+    return 0;
 }
